184314bca3ce79d633734b22151df50e213fe874
dany550 <dany.valek@seznam.cz>
2025-06-07 15:29:12 +0200

    homo

--- spike/main.py
+++ spike/main.py
@@ -1,11 +1,15 @@
 from bear_rescue import *
 from imgs import *
 from screen import *
+from roadside import *
+
 
 menu = Screen(drive.robot.hub)
 menu.addPage(Page(bear_rescue, icon=beari, image=arrow, delta=110))
 menu.addPage(Page(rotate, icon= fish, image = [fish,fish1], delta = 500))
 menu.addPage(Page(lambda: play(megalovania, 0.95), icon=smile, image=[skull, skull,skull, skull, skull2], delta=500))
+menu.addPage(Page(Roadmain, icon=road))
+
 
 menu.start()
 while True:
--- spike/roadside.py
+++ spike/roadside.py
@@ -3,25 +3,48 @@ from pybricks.tools import wait
 from tester import *
 
 #setup class
-def hook_align():
-    drive.robot.devices[0].run(100)
-    drive.robot.devices[1].run(-100)
+def hook_align(speed = 500):
+    drive.robot.devices[0].setSpeed(-speed)
+    drive.robot.devices[1].setSpeed(speed)
     wait(1000)  # wait for hooks to align
-    drive.device[0].stop()
-    drive.device[1].stop()
+    drive.robot.devices[0].stop()
+    drive.robot.devices[1].stop()
     drive.setMotorsToDef()
 
-def roadside_setup():
+def side_decider():
+    page = 0    
+    while True:
+        if drive.robot.hub.isButtonPressed(Button.LEFT):
+            page = -1
+            drive.robot.hub.beep(400, 250)
+            drive.robot.hub.color(Color.RED)
+        elif drive.robot.hub.isButtonPressed(Button.RIGHT):
+            page = 1
+            drive.robot.hub.beep(800, 250)
+            drive.robot.hub.color(Color.BLUE)
+        elif drive.robot.hub.isButtonPressed(Button.CENTER):
+            drive.robot.hub.beep(600, 500)
+            print("page", page)
+            return page
+
+def roadside_setup(side):
     drive.robot.devices.append(ColorSensor(Port.C)) #??? color sensor class???
-    drive.side = 1 # 1 = blue, -1 = red
     drive.hooks = [False, False]
-    if drive.side == 1:
+    if side == 1:
         drive.side_color = Color.BLUE
+        drive.side = 1
+        print("Blue side selected")
     else:
         drive.side_color = Color.RED
-    ###měnič orientace souřadnicového pole, měnič orientace robota
-    drive.robot.pos = vec2(12, 50) # reset position
-    hook_align()
+        drive.side = -1
+        print("Red side selected")
+        #drive.robot.lM.reverse = False
+        #drive.robot.rM.reverse = True
+    drive.robot.pos = vec2(11 * drive.side, 33) # reset position
+    drive.robot.hub.resetAngle()
+    drive.robot.hub.addOffset(180)  # set angle offset
+    print(drive.robot.pos, drive.robot.hub.angle())
+    hook_align(-500)
 
 #sken class
 def enemy_sken(ang = 0, val = 150): 
@@ -33,18 +56,16 @@ def enemy_sken(ang = 0, val = 150):
             return False
     return True
         
-    
-
 def car_sken(distance = 5): ##otestovat!
     for i in range (4):
-        drive.straight(distance)
+        drive.straight(distance, backwards=True)
         if drive.robot.devices[3].color() == drive.side_color:
             return i
 
 #hook class
 def hook_pickup(): ##otestovat!
-    drive.robot.devices[0].target(100)
-    drive.robot.devices[1].target(-100)
+    drive.robot.devices[0].target(500)
+    drive.robot.devices[1].target(-500)
     drive.hooks = [True, True]
 
 def hook_drop(N: int):
@@ -53,7 +74,7 @@ def hook_drop(N: int):
     drive.hooks[N] = False
 
 def battery_pickup(pickup_pos):
-    drive.toPos(vec2(pickup_pos.x, pickup_pos.y - 5))
+    drive.toPos(vec2(pickup_pos.x  * drive.side, pickup_pos.y - 5), speed = cspeed, backwards=True)
     drive.rotate(-90)
     drive.straight(5)
     hook_pickup()
@@ -67,22 +88,27 @@ def battery_delivery(car_number, Starting_pos = vec2(100, 40), car_distance = 5,
         hook = hook_shift
     else:
         return False
-    drive.toPos(vec2(Starting_pos.x + car_number * car_distance + hook, Starting_pos.y))
+    drive.toPos(vec2(Starting_pos.x + car_number * car_distance + hook, Starting_pos.y), speed = cspeed, backwards=True)
     hook_drop(N)
     return(True)
 
 #main class
+cspeed = 800
 def m1():
     #start
-    drive.circleToPos(vec2(50, 90), connect=[False, True])
-    drive.toPos(vec2(50, 180), connect=[True, False])
-    ###ninja moves
-    drive.rotate(180)
+    #drive.circleToPos(vec2(50, 70), connect=[False, True], speed = cspeed, backwards=True)
+    #drive.straight(5, backwards=False, speed = cspeed)
+    
+    drive.toPos(vec2(60 * drive.side, 30), speed = cspeed, backwards=True)
+    
+    drive.toPos(vec2(50 * drive.side, 160), backwards=False, speed = cspeed)
+    #ninja moves
+    drive.rotate(220)
     #zarovnání zpět
     ###?
-    drive.toPos(vec2(50, 10), backwards=True)
+    drive.toPos(vec2(50 * drive.side, 15), backwards = False)
     #nájezd na křižovatku m1 m2
-    drive.circleToPos(vec2(100, 40), connect=[False, True])
+    drive.circleToPos(vec2(100  * drive.side, 40), connect=[False, True], speed = cspeed, backwards=True)
 
 def m2():
     #nájezd na auta
@@ -97,13 +123,15 @@ def m3():
     pass
 
 def mF():
-    drive.toPos(vec2(20, 30))
+    drive.toPos(vec2(20 * drive.side, 30))
 
 #main 
-roadside_setup()
-m1()
-if enemy_sken() == False:
-    m2()
-else:
-    m3()
-mF()
+def Roadmain():    
+    roadside_setup(side_decider())
+    m1()
+    #if enemy_sken() == False:
+    #    hook_align()
+    #    m2()
+    #else:
+    #    m3()
+    mF()
